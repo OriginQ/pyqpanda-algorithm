f691fba706896c312ee004716e9368bdcca6214d
情尘 <174109236+wwwwww344@users.noreply.github.com>
2026-04-29 21:30:59 +0800

    feat(QSVM): add non-Hermitian quantum kernel with gain/loss operators

--- pyqpanda-algorithm/pyqpanda_alg/QSVM/__init__.py
+++ pyqpanda-algorithm/pyqpanda_alg/QSVM/__init__.py
@@ -6,3 +6,17 @@ reference: `Supervised learning with quantum enhanced feature spaces <https://ar
 from .quantum_kernel_svm import QuantumKernel_vqnet
 
 __all__ = ['QuantumKernel_vqnet']
+from .non_hermitian_quantum_kernel import (
+    NonHermitianQuantumKernel,
+    PTPhaseAnalyzer,
+    SupervisedGammaScheduler,
+    GammaScheduler,
+    symmetrize_and_psd,
+)
+__all__ += [
+    'NonHermitianQuantumKernel',
+    'PTPhaseAnalyzer',
+    'SupervisedGammaScheduler',
+    'GammaScheduler',
+    'symmetrize_and_psd',
+]
