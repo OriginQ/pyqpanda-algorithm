f10c0dd505db95d4213cc2c56687d87ba878e63e
qiu xiang <1826460695@qq.com>
2026-07-09 17:49:52 +0800

    Fix QAE target indexing bugs

--- test/QAlgBase/Test_QAE_QAE.py
+++ test/QAlgBase/Test_QAE_QAE.py
@@ -1,35 +1,74 @@
-# import math
-#
-# import pytest
-# import numpy as np
-# from pyqpanda_alg.QAE import QAE
-# from pyqpanda3.core import QCircuit, RY, X, H
-#
-#
-# def create_cir_basic(qlist):
-#     """基础测试量子线路"""
-#     cir = QCircuit()
-#     cir << RY(qlist[0], np.pi / 3) << X(qlist[1]).control(qlist[0])
-#     return cir
-#
-#
-# class Test_QAE_QAE:
-#
-#     def test_qae_basic_functionality(self):
-#         W = QAE(
-#             operator_in=create_cir_basic,
-#             qnumber=2,
-#             epsilon=0.01,
-#             res_index=[0, 1],
-#             target_state='11'
-#         ).run()
-#
-#         assert W is not None, "振幅估计结果不应为None"
-#         assert isinstance(W, (float, np.floating)), f"振幅应为数值类型，实际为{type(W)}"
-#         assert 0.2 <= W <= 0.3, f"振幅应在[0,1]范围内，实际为{W}"
-#
-#
-#
-# if __name__ == "__main__":
-#     # 运行所有测试
-#     pytest.main([__file__, "-v", "-s"])
+import numpy as np
+from pyqpanda3.core import QCircuit, RY
+
+from pyqpanda_alg.QAE import QAE
+
+
+def single_qubit_state(qlist):
+    cir = QCircuit()
+    cir << RY(qlist[0], np.pi / 3)
+    return cir
+
+
+def asymmetric_two_qubit_state(qlist):
+    cir = QCircuit()
+    cir << RY(qlist[0], np.pi / 3)
+    cir << RY(qlist[1], np.pi / 5)
+    return cir
+
+
+def test_qae_single_qubit_target_states():
+    prob_one = QAE(
+        operator_in=single_qubit_state,
+        qnumber=1,
+        epsilon=0.005,
+        res_index=0,
+        target_state="1",
+    ).run()
+    prob_zero = QAE(
+        operator_in=single_qubit_state,
+        qnumber=1,
+        epsilon=0.005,
+        res_index=0,
+        target_state="0",
+    ).run()
+
+    assert np.isclose(prob_one, 0.25, atol=0.02)
+    assert np.isclose(prob_zero, 0.75, atol=0.02)
+
+
+def test_qae_target_state_follows_res_index_order():
+    p0 = np.sin(np.pi / 6) ** 2
+    p1 = np.sin(np.pi / 10) ** 2
+    expected = {
+        "00": (1 - p0) * (1 - p1),
+        "01": (1 - p0) * p1,
+        "10": p0 * (1 - p1),
+        "11": p0 * p1,
+    }
+
+    for target_state, target_prob in expected.items():
+        prob = QAE(
+            operator_in=asymmetric_two_qubit_state,
+            qnumber=2,
+            epsilon=0.005,
+            res_index=[0, 1],
+            target_state=target_state,
+        ).run()
+
+        assert np.isclose(prob, target_prob, atol=0.02)
+
+
+def test_qae_non_natural_res_index_order():
+    p0 = np.sin(np.pi / 6) ** 2
+    p1 = np.sin(np.pi / 10) ** 2
+
+    prob = QAE(
+        operator_in=asymmetric_two_qubit_state,
+        qnumber=2,
+        epsilon=0.005,
+        res_index=[1, 0],
+        target_state="01",
+    ).run()
+
+    assert np.isclose(prob, (1 - p1) * p0, atol=0.02)
